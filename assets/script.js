6865bb13ce32a74a57ad9c7261cdc3abce417fe6
denis-mwangi <dennism7071@gmail.com>
2025-03-31 19:23:11 +0300

    fixed some bugs

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,36 @@
 // TYPING ANIMATION
-var typed = new Typed(".typing", {
-    strings:["","Web Designer","web Developer","Graphic Designer","Youtuber"],
-    typespeed:1000,
-    BackSpeed:100,
-    loop:true
-})
+
+let typed = new Typed('.typing', {
+  strings: ['','Web Developer', 'Front-end Designer', "Python Developer", "C++ Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+// ASIDE MENU
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function() {
+          for(let j=0; j<totalNavList; j++){
+            navList[j].querySelector("a").classList.remove("active");
+          }
+          this.classList.add("active")
+          showSection(this);
+        })
+      }
+      function showSection(element){
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+      }
+      // TOGGLE NAV BAR FOR MOBILE PHONES
+      const navToggleBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navToggleBtn.addEventListener("click", () => {
+              asideSectionToggleBtn();
+            })
+            function asideSectionToggleBtn(){
+              aside.classList.toggle("open");
+            }
